68b2c3c5a8898e187c8e7260a6ad861eb927ea98
Icaro Davi <icarodaviduarte@gmail.com>
2022-06-20 01:36:39 -0300

    Added footer

---
+++ src/components/layout/Footer/styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledFooterContainer = styled.footer`
+    height: 5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.3rem;
+`;
