9126ccb1da38637acc3c2f97ecca8492d46783ed
Icaro Davi <icarodaviduarte@gmail.com>
2022-07-21 18:46:59 -0300

    get user guilds

--- src/components/layout/Auth/index.tsx
+++ src/components/layout/Auth/index.tsx
@@ -1,10 +1,21 @@
 import React, { ReactNode } from "react";
+import { useEffect } from "react";
+import { useApp } from "../../../context/App";
+import DiscordUserService from "../../../services/discord/user";
 
 interface IAuthLayout {
     children: ReactNode;
 }
 
 const AuthLayout: React.FC<IAuthLayout> = props => {
+    const { store: { guilds }, dispatchStore } = useApp();
+    useEffect(() => {
+        let cancelRequest = false;
+        !guilds.length && DiscordUserService.getGuilds().then(guilds => {
+            if (!cancelRequest) dispatchStore({ type: 'SET_GUILDS', payload: { guilds } });
+        });
+        return () => { cancelRequest = true };
+    }, []);
     return (
         <React.Fragment>
             <div style={{ backgroundColor: 'red', width: '100%', height: '100%' }}>
