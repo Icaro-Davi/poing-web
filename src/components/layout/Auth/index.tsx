31449d3df41b94fb4b3398daa50e78ee08e47ea5
Icaro Davi <icarodaviduarte@gmail.com>
2022-07-23 19:13:43 -0300

    feat: get guilds

--- src/components/layout/Auth/index.tsx
+++ src/components/layout/Auth/index.tsx
@@ -1,29 +1,33 @@
-import React, { ReactNode } from "react";
+import { FC, ReactNode, memo } from "react";
 import { useEffect } from "react";
+
 import { useApp } from "../../../context/App";
 import DiscordUserService from "../../../services/discord/user";
+import SideMenu from "./SideMenu";
+import { Container, Main } from "./styles";
 
 interface IAuthLayout {
     children: ReactNode;
 }
 
-const AuthLayout: React.FC<IAuthLayout> = props => {
+const AuthLayout: FC<IAuthLayout> = props => {
     const { store: { guilds }, dispatchStore } = useApp();
     useEffect(() => {
         let cancelRequest = false;
-        !guilds.length && DiscordUserService.getGuilds().then(guilds => {
-            if (!cancelRequest) dispatchStore({ type: 'SET_GUILDS', payload: { guilds } });
-        });
+        !guilds.length && DiscordUserService.getGuilds()
+            .then(guilds => {
+                if (!cancelRequest) dispatchStore({ type: 'SET_GUILDS', payload: { guilds } });
+            });
         return () => { cancelRequest = true };
     }, []);
     return (
-        <React.Fragment>
-            <div style={{ width: '100%', height: '100%' }}>
-                Authenticated
+        <Container>
+            <SideMenu />
+            <Main>
                 {props.children}
-            </div>
-        </React.Fragment>
+            </Main>
+        </Container>
     );
 }
 
-export default React.memo(AuthLayout);
+export default memo(AuthLayout);
---
+++ src/components/layout/Auth/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: gray;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-flow: row;
+`;
+
+export const Main = styled.main`
+    display: flex;
+    flex: 1;
+    background-color: #fff;
+    padding: 1em;
+    padding-bottom: 0;
+    overflow: hidden;
+`;
