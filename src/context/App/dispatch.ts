d6ab075d03388b0d90c30fc1249304b90cd5ec30
Icaro Davi <icarodaviduarte@gmail.com>
2022-07-26 10:58:26 -0300

    feat: handle app dispatch

--- src/context/App/dispatch.ts
+++ src/context/App/dispatch.ts
@@ -1,14 +1,25 @@
 import { AppDispatchStore } from "./app.types";
 import DiscordUserService from "../../services/discord/user";
+import LocalStorage from "../../utils/localStorage";
 
 class AppDispatch {
     static async findGuildAndSave(dispatch: AppDispatchStore) {
         try {
             const guilds = await DiscordUserService.getGuilds();
-            dispatch({ type: 'SET_GUILDS', payload: { guilds } });
+            if (guilds) {
+                dispatch({ type: 'SET_GUILDS', payload: { guilds } });
+                const selectedGuildId = LocalStorage.guild.getSelectedId() || guilds[0].id;
+                dispatch({ type: 'SET_SELECTED_GUILD', payload: { selectedGuildId } });
+                LocalStorage.guild.setSelectedId(selectedGuildId);
+            }
         } catch (error) {
             throw error;
         }
+    }
+
+    static async setSelectedGuildId(dispatch: AppDispatchStore, selectedGuildId: string) {
+        dispatch({ type: 'SET_SELECTED_GUILD', payload: { selectedGuildId } });
+        LocalStorage.guild.setSelectedId(selectedGuildId);
     }
 }
 
