f23c268e6b673f7510afa48cae5b62b243089590
Icaro Davi <icarodaviduarte@gmail.com>
2022-07-29 14:33:27 -0300

    change: invite bot btn

---
+++ src/components/Buttons/InviteBot.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import { ButtonShining } from ".";
+import { useApp } from "../../context/App";
+import { SiDiscord } from 'react-icons/si';
+
+const InviteBotBtn: FC = props => {
+    const { locale: { pages: { home: { welcomeCard } } } } = useApp();
+    return (
+        <ButtonShining
+            style={{ position: 'relative', marginTop: '0.5rem', marginBottom: '1rem', display: 'flex', alignItems: 'center', columnGap: '.5em' }}
+            onClick={() => window.open(process.env.NEXT_PUBLIC_BOT_INVITE_URL, '_blank')?.focus()}
+        ><SiDiscord /> {welcomeCard.buttonBotInvitation}</ButtonShining>
+    )
+}
+
+export default InviteBotBtn;
