af433864e29e0594639632d78881d2b82859ac18
Icaro Davi <icarodaviduarte@gmail.com>
2022-07-29 14:43:49 -0300

    Change: Profile background align

--- src/components/Profile/index.ts
+++ src/components/Profile/index.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
-export const Profile = styled.div<{ imageSrc: string }>`
-    width: clamp(80px, 5vw, 100px);
-    height: clamp(80px, 5vw, 100px);
+export const Profile = styled.div<{ imageSrc: string, size?: number }>`
+    width: ${props => props.size ? `${props.size}px` : 'clamp(80px, 5vw, 100px)'};
+    height: ${props => props.size ? `${props.size}px` : 'clamp(80px, 5vw, 100px)'};
     background-image: url(${props => props.imageSrc});
     background-size: cover;
+    background-position: center;
     border-radius: 100%;
 `;
