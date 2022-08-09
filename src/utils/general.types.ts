fd3a6c0745baa0aea7db64a1b96e18202dd8b06a
Icaro Davi <icarodaviduarte@gmail.com>
2022-08-08 21:48:57 -0300

    change: new util type

--- src/utils/general.types.ts
+++ src/utils/general.types.ts
@@ -3,6 +3,7 @@ import { ReactElement, ReactNode } from 'react';
 import { AppTheme } from "../styles/themes/default";
 
 export type PickKeys<T, Key extends keyof T> = keyof T[Key];
+export type GetReference<T, Key extends keyof T> = T[Key];
 export type PickInside<Type extends { [k: string]: any }, Key extends keyof Type> = Type[Key][any];
 export type ThemeColorsKeys = PickKeys<AppTheme, 'colors'>;
 
