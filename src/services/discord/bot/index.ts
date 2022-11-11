dfd9824c2d4c19f145976d619bdc50159b174532
Icaro Davi <icarodaviduarte@gmail.com>
2022-11-11 12:58:34 -0300

    change: save bot settings in local storage

--- src/services/discord/bot/index.ts
+++ src/services/discord/bot/index.ts
@@ -1,17 +1,25 @@
 import DiscordRequestor from "../requestor";
 import type { GetReference } from "../../../utils/general.types";
 import type { GuildSettingsType } from "./bot.types";
+import LocalStorage from "../../../utils/localStorage";
 
 class DiscordBotService {
     static basePath = '/bot';
     static async getGuildSettingsById(guildId: string) {
         try {
-            const { data } = await DiscordRequestor.get<GuildSettingsType>(`${this.basePath}/guild/${guildId}`);
-            return data;
+            const botSettings = LocalStorage.bot.getSettings();
+            if ((botSettings && (!botSettings?.nextFetch || botSettings.nextFetch < Date.now())) || !botSettings || guildId !== botSettings._id) {
+                const { data } = await DiscordRequestor.get<GuildSettingsType>(`${this.basePath}/guild/${guildId}`);
+                LocalStorage.bot.setSettings(data, { updateFetchDate: true });
+                return data;
+            } else {
+                return botSettings;
+            }
         } catch (error) {
             throw error;
         }
     }
+
     static async updateGuildSettingsById(guildId: string, settings: Omit<GetReference<GuildSettingsType, 'bot'>, 'roles'>) {
         try {
             await DiscordRequestor.post(`${this.basePath}/guild/${guildId}`, settings);
@@ -19,7 +27,6 @@ class DiscordBotService {
             throw error;
         }
     }
-
 }
 
 export default DiscordBotService;
